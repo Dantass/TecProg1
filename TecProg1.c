291a178b34a093ce6d277548d73f637c5adcb03b
Dantass <57996434+Dantass@users.noreply.github.com>
2020-03-04 21:27:32 -0300

    Add files via upload

--- TecProg1.c
+++ TecProg1.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*Linguagem de alto nivel -> blueprint(Programaçao em blocls)*/
+int a = 0;
+int main() {
+	//1. Declarar as variáveis
+	int anos;
+	int meses;
+	
+	//2. Pegar os valores do usario (input)
+	printf("Digite os anos:\n");
+	scanf("%d",&anos);
+	// \n quebra para linha de baixo
+	// o & indica a memória onde o valor será armazenado
+	//&anos é um endereço de mémoria, onde armazena o valor
+	//scanf precisa de 2 argumentos
+	//"%d" string, place holder do tipo inteiro
+	
+	printf("%d \n", anos);
+	
+	//3. Fazer o calculo
+	printf("%d", anos*12);
+	
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
 	return 0;
 }
-aaaaaaaa
+
