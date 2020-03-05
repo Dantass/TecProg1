c7e71a4329e0e7a1bf1eea394787fa0d87d07e00
Dantass <57996434+Dantass@users.noreply.github.com>
2020-03-05 10:29:31 -0300

    Add files via upload

---
+++ area_retângulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+int base;
+int altura; 
+printf("Digite o valor da base do retangulo: \n");
+scanf("%d", &base);
+printf("Digite o valor da altura do retangulo: \n");
+scanf("%d", &altura);
+printf("A area do retangulo possui %d unidades de medida",base*altura);	
+
+	return 0;
+}
